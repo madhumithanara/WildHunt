92747ca590cd54ea98794f5de94474fcbbf0c25a
harshnitk17 <harshprakashgupta.171it116@nitk.edu.in>
2020-01-22 00:39:42 +0530

    correction in sigin

--- sih/sih/urls.py
+++ sih/sih/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
 #from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from django.urls import path, include
 
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
