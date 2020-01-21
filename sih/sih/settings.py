104e62e4eadd627d1c0fa4c9179e5311889d5334
madhumithanara <madhumithanara.171it125@nitk.edu.in>
2020-01-22 00:56:33 +0530

    Added basic frontend - home page and profile page

--- sih/sih/settings.py
+++ sih/sih/settings.py
@@ -117,8 +117,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
