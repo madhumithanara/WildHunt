ae6e93ffb8284cfcd8f0f41fe11ba4f95cfa5ce1
Siddharth Singh <siddharth12375@gmail.com>
2020-01-22 11:19:08 +0530

    Update ndviscript.py

--- ndviscript.py
+++ ndviscript.py
@@ -28,6 +28,9 @@ def fun(impath):
   results_kharif = []
   results_zaid = []
   results_rabi = []
+  results_kharife = []
+  results_zaide = []
+  results_rabie = []
 
   time = []
   from matplotlib import colors
@@ -36,6 +39,9 @@ def fun(impath):
     pixels_kharif = 0
     pixels_zaid = 0
     pixels_rabi = 0
+    pixels_kharif2 = 0
+    pixels_zaid2 = 0
+    pixels_rabi2 = 0
     time.append(i//2)
     print(filenames[i])
     print(filenames[i+1])
@@ -79,6 +85,26 @@ def fun(impath):
     results_kharif.append(pixels_kharif)
     results_rabi.append(pixels_rabi)
     results_zaid.append(pixels_zaid)
+    #evi2 calculation, empty cells or nodata cells are reported as 0
+    evi2=np.where(
+        (nir+red)==0., 
+        0, 
+        2.5*((nir-red)/(nir+2.4*red+1)))
+    evi2[:5,:5]
+    shape = evi2.shape
+    for ii in range(0,shape[0]):
+        for j in range(0,shape[1]):
+            # if(evi2[ii][j]<0.2 or evi2[ii][j] > 0.6):
+            #     evi2[ii][j]=-1
+            if((evi2[ii][j]>=0.84 and evi2[ii][j] <= 1.25)):
+              pixels_kharif += 1
+            if((evi2[ii][j]>=0.45 and evi2[ii][j] <= 0.72)):
+              pixels_rabi += 1
+            if((evi2[ii][j]>=0.55 and evi2[ii][j] <= 0.96)):
+              pixels_zaid += 1
+    results_kharife.append(pixels_kharif2)
+    results_rabie.append(pixels_rabi2)
+    results_zaide.append(pixels_zaid2)
     
     #export ndvi image
     ndviImage = rasterio.open('../ndviImage'+str(i) +'.tiff','w',driver='Gtiff',
@@ -100,12 +126,15 @@ def fun(impath):
   os.chdir('../')  
   print (str(os.path.dirname(os.path.realpath(__file__))))
   return str(os.path.dirname(os.path.realpath(__file__)))
-  plt.plot(time, results_kharif, label="NDVI_kharif")
-  plt.savefig('results_kharif.png',dpi=300)
+  plt.plot(time, results_kharif, label="NDVI_kharif")  
   plt.plot(time, results_rabi, label="NDVI_rabi")
-  plt.savefig('results_rabi.png',dpi=300)
   plt.plot(time, results_zaid, label="NDVI_zaid")
-  plt.savefig('results_zaid.png',dpi=300)
   plt.legend(loc=2)
-  plt.show()
-fun("../sih/Clipped_NDVI/")
+  plt.savefig('ndvicomp.png',dpi=300)
+  plt.plot(time, results_kharife, label="EVI2_kharif")
+  plt.plot(time, results_rabie, label="EVI2_rabi")
+  plt.plot(time, results_zaide, label="EVI2_zaid")
+  plt.legend(loc=2)
+  plt.savefig('evicomp.png',dpi=300)
+  
+#fun("../sih/Clipped_NDVI/")
